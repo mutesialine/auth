24a0c3ec0f77c292fd00aca2276a1c9b4977c643
mutesialine <ktesialine@gmail.com>
2023-09-05 12:37:58 +0200

    feat: auth routes and controllers

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,10 @@
+const {Router}= require('express')
+const {signup_get, signup_post,login_post,login_get} =require('../controllers/authController')
+const router= Router()
+
+router.get('/signup', signup_get)
+router.post('/signup',signup_post)
+router.get('/login', login_get)
+router.post('/login', login_post)
+
+module.exports = router

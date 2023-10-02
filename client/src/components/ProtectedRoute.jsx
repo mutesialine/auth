76699fd79e455df835a584a2b305ce1d0576110b
mutesialine <ktesialine@gmail.com>
2023-10-02 22:26:08 +0200

    fix:the protected route

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,14 +1,14 @@
-import { Route } from "react-router-dom";
-import { useAuthProtection } from "../util/auth";
-import Tote from "../pages/Tote";
-import Login from "../pages/Login";
+// import { Route } from "react-router-dom";
+// import { useAuthProtection } from "../util/auth";
+// import Tote from "../pages/Tote";
+// import Login from "../pages/Login";
 
-export default function ProtectedRoute() {
-  const isAuthenticatedUser = useAuthProtection();
+// export default function ProtectedRoute() {
+//   const isAuthenticatedUser = useAuthProtection();
 
-  return isAuthenticatedUser ? (
-    <Route path="/tote" element={<Tote />} />
-  ) : (
-    <Route path="/login" element={<Login />} />
-  );
-}
+//   return isAuthenticatedUser ? (
+//     <Route path="/tote" element={<Tote />} />
+//   ) : (
+//     <Route path="/login" element={<Login />} />
+//   );
+// }
